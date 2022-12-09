f0f19cbb12969f7c5c456b0fad4323543d165477
Jorge Carballo <jorge@molmo.es>
2022-12-09 17:15:22 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "             cloning zsh-syntax-highlighting               "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "==========================================================="
+    echo "             import zshrc                                  "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
+
+zshrc
+
+# make directly highlighting readable - needs to be after zshrc line
+echo "" >> ~/.zshrc
+echo "# remove ls and directory completion highlight color" >> ~/.zshrc
+echo "_ls_colors=':ow=01;33'" >> ~/.zshrc
+echo 'zstyle ":completion:*:default" list-colors "${(s.:.)_ls_colors}"' >> ~/.zshrc
+echo 'LS_COLORS+=$_ls_colors' >> ~/.zshrc
+
+# set gitconfig defaults
+git config --global push.autoSetupRemote true
